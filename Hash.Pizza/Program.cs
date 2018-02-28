f8692577431317158f135b26e72a062eb04aabe8
Maarten Claes <maarten.claes@cegeka.com>
2018-02-28 22:37:28 +0100

    FInal commit (produces a wrong output....)

--- Hash.Pizza/Program.cs
+++ Hash.Pizza/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Hash.Pizza.SliceValidator;
 
 namespace Hash.Pizza
 {
@@ -27,7 +29,7 @@ namespace Hash.Pizza
         private int HighestAmount { get; set; }
 
         // Result
-        private int[,] Slices { get; set; }
+        private List<int[]> Slices { get; set; }
 
         public PizzaScanner(string input)
         {
@@ -50,20 +52,81 @@ namespace Hash.Pizza
             Pizza = new char[RowsMax, ColsMax];
 
             for (var l = 0; l < lines.Length; l++)
-                for (var c = 0; c < lines[l].Length; c++)
-                    Pizza[l, c] = lines[l][c];
+            for (var c = 0; c < lines[l].Length; c++)
+                Pizza[l, c] = lines[l][c];
         }
+
+        class Shape
+        {
+            public int width { get; set; }
+            public int height { get; set; }
+        }
+
+        public class Shapes
+        {
+            private Dictionary<int, IEnumerable<Shape>> ShapesBySize { get; set; }
+
+            public Shapes(int r, int c, int l, int h)
+            {
+                ShapesBySize = Enumerable.Range(l * 2, h - 1).ToDictionary(i => i, hCounter =>
+                {
+                    var shapes = new List<Shape>();
+
+                    var ss = Enumerable.Range(2, hCounter).Where(counter => hCounter % counter == 0 && counter <= h);
+
+                    shapes.AddRange(ss.Select(counter => new Shape { H = counter, W = (int)Math.Floor(hCounter / (float)counter) }).ToList());
+
+                    var revsersed = shapes.Where(s => s.H != s.W).Select(s => new Shape { H = s.W, W = s.H }).ToList();
+                    shapes.AddRange(revsersed);
+
+                    return shapes.AsEnumerable();
+                });
+            }
+        }
+
 
         public void Scan()
         {
-            Slices = new[,] { {0,0,2,1}, {0,2,2,2}, {0,3,2,4} };
+            ISliceValidator validator = new SliceValidatorVOne();
+            Slices = new List<int[]>();
+
+            int shapeIndex = 0;
+            var shapes = new[]
+            {
+                new Shape {width = 3, height = 2},
+                new Shape {width = 2, height = 3},
+                new Shape {width = 3, height = 1},
+                new Shape {width = 1, height = 3},
+            };
+
+            int rowStep = 1;
+            for (int r = 0; r < RowsMax;)
+            {
+                for (int c = 0; c < ColsMax;)
+                {
+                    if (r + shapes[shapeIndex].height < RowsMax &&
+                        c + shapes[shapeIndex].width < ColsMax &&
+                        validator.SliceIsValid(Pizza, r, r + shapes[shapeIndex].height, c,
+                            c + shapes[shapeIndex].width,
+                            LowestAmount, HighestAmount, 2))
+                    {
+                        Slices.Add(new[] {r, c, r + shapes[shapeIndex].height, c + shapes[shapeIndex].width});
+                        c += shapes[shapeIndex].width;
+                        rowStep = shapes[shapeIndex].height;
+                    }
+                    else
+                        c++;
+                }
+
+                r += rowStep;
+            }
         }
 
         public void WriteResult()
         {
-            var output = Slices.Length + Environment.NewLine;
-            for (var i = 0; i < Slices.Length / 4; i++)
-                output += $"{Slices[i,0]} {Slices[i,1]} {Slices[i,2]} {Slices[i,3]} {Environment.NewLine}";
+            var output = Slices.Count + Environment.NewLine;
+            foreach (var slice in Slices)
+                output += $"{slice[0]} {slice[1]} {slice[2]} {slice[3]} {Environment.NewLine}";
             Console.Write($"Result:{Environment.NewLine}{output}");
             File.WriteAllText("output.txt", output);
         }
