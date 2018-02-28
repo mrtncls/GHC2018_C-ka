5561b207e69f7b2a203b7119c2e1535b1743dba2
Maarten Claes <maarten.claes@cegeka.com>
2018-02-28 20:32:40 +0100

    First version

---
+++ Hash.Pizza/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Hash.Pizza
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var scanner = new PizzaScanner();
+            scanner.Scan();
+            Console.ReadLine();
+        }
+    }
+
+    public class PizzaScanner
+    {
+        // Dataset
+        private char[][] Pizza { get; set; }
+
+        // Configuration
+        private int RowsMax { get; set; }
+        private int ColsMax { get; set; }
+        private int LowestAmount { get; set; }
+        private int HighestAmount { get; set; }
+
+        public PizzaScanner(string input)
+        {
+            ReadInput(input);
+        }
+
+        private void ReadInput(string input)
+        {
+            var lines = File.ReadAllLines(input);
+
+            var confRow = lines.First().Split(' ');
+            RowsMax = int.Parse(confRow[0]);
+            ColsMax = int.Parse(confRow[1]);
+            LowestAmount = int.Parse(confRow[2]);
+            HighestAmount = int.Parse(confRow[3]);
+            Console.WriteLine($"R: {RowsMax} C: {ColsMax} L: {LowestAmount} H: {HighestAmount}");
+
+            const int counter = 0;
+            foreach (var line in lines.Skip(1))
+            {
+                var ingredients = line.ToCharArray();
+                Pizza[counter] = ingredients;
+            }
+        }
+
+        public void Scan()
+        {
+
+        }
+    }
+}
